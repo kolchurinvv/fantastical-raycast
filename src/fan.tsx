e80d47f9191e8189614b4f1d31ab695b5efbc1b7
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2022-08-01 22:34:25 +0200

    change to no view and run hardcoded value

--- src/fan.tsx
+++ src/fan.tsx
@@ -1,1 +1,5 @@
-export {}
+import {runAppleScript} from 'run-applescript';
+
+export default async () => {
+  await runAppleScript("tell application \"Fantastical\" \n parse sentence \"Wake up at 8am\" \n end tell");
+};
