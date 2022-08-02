a2f9930e58a1d8d9b6fab5a8568b605c1c09e569
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2022-08-02 08:45:30 +0200

    getting ready to publish

--- src/isInstalled.tsx
+++
@@ -1,8 +0,0 @@
-import { getApplications } from '@raycast/api';
-
-export async function isInstalled() {
-    const isInstalled = (await getApplications()).find(app => app.name === 'Fantastical') != undefined ? true : false;
-    return isInstalled;
-}
-
-export {}
