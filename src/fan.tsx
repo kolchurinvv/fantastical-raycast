6820dcb46f9e6b5598c8e845725ed07dd679b580
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2022-08-02 03:53:49 +0200

    now takes user input through argument

--- src/fan.tsx
+++ src/fan.tsx
@@ -4,7 +4,6 @@ interface Arguments {
     add: string;
 }
 
-
 export default async (props: { arguments: Arguments }) => {
     const args = props.arguments;
     const text = `tell application \"Fantastical\" \n parse sentence \"${args.add}\" \n end tell`
