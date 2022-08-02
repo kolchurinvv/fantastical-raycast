e1f8e31f480d0dd487f6841cba3817bf7fbad6ee
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2022-08-02 07:51:02 +0200

    added is installed function

--- src/fan-reminder.tsx
+++ src/fan-reminder.tsx
@@ -1,4 +1,6 @@
+import { getApplications } from '@raycast/api';
 import {runAppleScript} from 'run-applescript';
+import { isInstalled } from './isInstalled';
 
 interface Arguments {
     add: string;
@@ -6,8 +8,12 @@ interface Arguments {
 
 export default async (props: { arguments: Arguments }) => {
     const args = props.arguments;
-    const text = `tell application \"Fantastical\" 
-                    \n parse sentence \"Todo ${args.add}\" 
-                \n end tell`
-    await runAppleScript(text);
+    if (await isInstalled()) {
+        const text = `tell application \"Fantastical\" 
+                        \n parse sentence \"Todo ${args.add}\" 
+                    \n end tell`
+        await runAppleScript(text);
+    } else {
+        console.log('Fantastical is not installed');
+    }
 };
--- src/fan.tsx
+++ src/fan.tsx
@@ -1,14 +1,18 @@
 import {runAppleScript} from 'run-applescript';
-
+import { isInstalled } from './isInstalled';
 interface Arguments {
     add: string;
 }
 
 export default async (props: { arguments: Arguments }) => {
     const args = props.arguments;
-    await runAppleScript(`
-    tell application \"Fantastical\" 
-        \n parse sentence \"${args.add}\" \n
-    end tell`
-    );
+    if (await isInstalled()) {
+        await runAppleScript(`
+        tell application \"Fantastical\" 
+            \n parse sentence \"${args.add}\" \n
+        end tell`
+        );
+    } else {
+        console.log('Fantastical is not installed');
+    }
 };
