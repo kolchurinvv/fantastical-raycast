d9d2b0de3e7aec59c61957eb7eac0fc71ab582e4
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2022-08-02 03:52:57 +0200

    now takes user input through

--- src/fan.tsx
+++ src/fan.tsx
@@ -1,5 +1,12 @@
 import {runAppleScript} from 'run-applescript';
 
-export default async () => {
-  await runAppleScript("tell application \"Fantastical\" \n parse sentence \"Wake up at 8am\" \n end tell");
+interface Arguments {
+    add: string;
+}
+
+
+export default async (props: { arguments: Arguments }) => {
+    const args = props.arguments;
+    const text = `tell application \"Fantastical\" \n parse sentence \"${args.add}\" \n end tell`
+    await runAppleScript(`tell application \"Fantastical\" \n parse sentence \"${args.add}\" \n end tell`);
 };
